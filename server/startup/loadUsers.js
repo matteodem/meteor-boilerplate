041488e5bb388d2a0a7c7e80e21cfea34d78baa9
Matteo De Micheli <mat.demicheli@gmail.com>
2013-10-25 20:02:06 +0200

    Remove reset users function

--- server/startup/loadUsers.js
+++ server/startup/loadUsers.js
@@ -6,16 +6,11 @@ function loadUser(user) {
     }
 }
 
-function resetUsers() {
-    Meteor.users.remove({});
-}
-
 Meteor.startup(function () {
     var i,
         users = YAML.eval(Assets.getText('users.yml'));
 
     for (key in users) {
-        //resetUsers();
         loadUser(users[key]);
     }
 });
