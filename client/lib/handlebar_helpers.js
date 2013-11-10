e34ba27ed58cfcfffe2042b2a2638603875b6b59
Matteo De Micheli <mat.demicheli@gmail.com>
2013-11-10 12:47:35 +0100

    Add a debug helper, thanks to rhyslbw

--- client/lib/handlebar_helpers.js
+++ client/lib/handlebar_helpers.js
@@ -7,3 +7,27 @@ Handlebars.registerHelper('isActive', function (siteName) {
 
     return 'not-active';
 });
+
+Handlebars.registerHelper('debug', function (optionalValue) {
+    if (typeof console !== "undefined" || typeof console.log !== "undefined") {
+        console.log("Current Context");
+        console.log("====================");
+        console.log(this);
+        if (optionalValue) {
+            console.log("Value");
+            console.log("====================");
+            console.log(optionalValue);
+        }
+
+        return '';
+    }
+
+    // For IE8
+    alert(this);
+
+    if (optionalValue) {
+        alert(optionalValue);
+    }
+
+    return '';
+});
