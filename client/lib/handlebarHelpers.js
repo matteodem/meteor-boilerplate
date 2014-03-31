15bf98c34f6f1f9b08a43c113fbb727ba0288c18
Matteo De Micheli <mat.demicheli@gmail.com>
2014-03-31 19:25:00 +0200

    Upgraded to 0.8.0 Ultra Awesome Blaze

--- client/lib/handlebarHelpers.js
+++ client/lib/handlebarHelpers.js
@@ -1,4 +1,4 @@
-Handlebars.registerHelper('isActive', function (siteName) {
+UI.registerHelper('isActive', function (siteName) {
     var current = Router.current();
 
     if ('undefined' !== typeof current && current !== null) {
@@ -8,7 +8,7 @@ Handlebars.registerHelper('isActive', function (siteName) {
     return 'not-active';
 });
 
-Handlebars.registerHelper('debug', function (optionalValue) {
+UI.registerHelper('debug', function (optionalValue) {
     if (typeof console !== "undefined" || typeof console.log !== "undefined") {
         console.log("Current Context");
         console.log("====================");
