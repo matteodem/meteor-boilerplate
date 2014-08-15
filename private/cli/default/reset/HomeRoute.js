e44d15e2ab6cf2d45a69b0d7cf2fec3604e84a75
Matteo De Micheli <mat.demicheli@gmail.com>
2014-08-15 17:09:34 +0200

    Commit for reset:project command, only default profile

---
+++ private/cli/default/reset/HomeRoute.js
@@ -0,0 +1,10 @@
+var HomeController = RouteController.extend({
+  template: 'home'
+});
+
+Router.map(function () {
+  this.route('home', {
+    path :  '/',
+    controller :  HomeController
+  });
+});
