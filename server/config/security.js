65896438c3963f3432eb5a69ab4d1d20abc88818
Matteo De Micheli <mat.demicheli@gmail.com>
2015-01-09 08:34:18 +0100

    Adjust browser policy settings

--- server/config/security.js
+++ server/config/security.js
@@ -1,3 +1,3 @@
-BrowserPolicy.content.allowOriginForAll("http://fonts.googleapis.com");
-BrowserPolicy.content.allowOriginForAll("http://fonts.gstatic.com");
-BrowserPolicy.content.allowOriginForAll("http://maxcdn.bootstrapcdn.com");
+BrowserPolicy.content.allowOriginForAll("*.googleapis.com");
+BrowserPolicy.content.allowOriginForAll("*.gstatic.com");
+BrowserPolicy.content.allowOriginForAll("*.bootstrapcdn.com");
