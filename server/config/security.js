b6182c2d9e2cfc7bb43d615a32a966b0a7a8dcf8
Matteo De Micheli <mat.demicheli@gmail.com>
2015-01-09 21:35:04 +0100

    Allow fonts from data: origins

--- server/config/security.js
+++ server/config/security.js
@@ -1,3 +1,5 @@
 BrowserPolicy.content.allowOriginForAll("*.googleapis.com");
 BrowserPolicy.content.allowOriginForAll("*.gstatic.com");
 BrowserPolicy.content.allowOriginForAll("*.bootstrapcdn.com");
+
+BrowserPolicy.content.allowFontDataUrl();
