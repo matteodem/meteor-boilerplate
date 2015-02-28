51a14a99c760fee298cdc3b387f73053defb835f
Matteo De Micheli <mat.demicheli@gmail.com>
2015-02-28 20:45:48 +0100

    Updated to latest Meteor version 1.0.3.2 and fixed SEO warning

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -1,5 +1,8 @@
 // Home Route
-Router.route('/', function () {
-  this.render('home');
-  SEO.set({ title: 'Home - ' + Meteor.App.NAME });
+Router.route('/', {
+  name: 'home',
+  action: function () {
+    this.render('home');
+    SEO.set({ title: 'Home - ' + Meteor.App.NAME });
+  }
 });
