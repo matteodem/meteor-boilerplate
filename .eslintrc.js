489903eb04fe8e83706589ccf58cbcbff452da24
Matteo De Micheli <matteodem@protonmail.com>
2017-06-23 21:51:09 +0200

    Make eslint run through

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,12 @@
 module.exports = {
+    "parser": "babel-eslint",
     "extends": "standard",
     "plugins": [
         "standard",
         "promise"
     ],
     "rules": {
+        "space-before-function-paren": "off",
         "comma-dangle": "off",
         "spaced-comment": "off"
     }
